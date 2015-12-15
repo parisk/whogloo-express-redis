1a102765d263ce841f44328877a3e1fe6b87ca25
Antonis Kalipetis <akalipetis@gmail.com>
2015-12-15 19:16:03 +0200

    Add counter to index route

--- greecejs_demo/routes/index.js
+++ greecejs_demo/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+
+var client = redis.createClient({host: 'redis', port: '6379'});
+
+client.on('connect', function() {
+    console.log('connected');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    client.get('counter', function(err, value) {
+        value = parseInt(value);
+        if (isNaN(value)) {
+            value = 0;
+        }
+        client.set('counter', value + 1);
+        res.render('index', {title: 'Express: ' + value});
+    });
 });
 
 module.exports = router;
